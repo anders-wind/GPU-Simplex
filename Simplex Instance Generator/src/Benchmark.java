9e13881529a3410436c6456cb921f4a11cd2838b
Anders Wind <awia00@gmail.com>
2017-11-02 17:40:36 +0100

    Small changes to simplex generator benchmark to hopefully have more precise results

--- Simplex Instance Generator/src/Benchmark.java
+++ Simplex Instance Generator/src/Benchmark.java
@@ -16,10 +16,9 @@ public class Benchmark {
      * type T and a timer. The function should time its critical portion using the timer.
      */
     public static <T> void benchmark(String msg, BiConsumer<T, Timer> f, T arg) {
-        int n = 3, count = 1, totalCount = 0;
+        int n = 5, count = 1, totalCount = 0;
         double runningTime = 0.0, st = 0.0, sst = 0.0;
         do {
-            count *= 2;
             st = sst = 0.0;
             for (int j=0; j<n; j++) {
                 Timer t = new Timer();
@@ -31,6 +30,7 @@ public class Benchmark {
                 sst += time * time;
                 totalCount += count;
             }
+            count *= 2;
         } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
         double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
         System.err.printf("%-25s mean: %15.1f us, stddev: %10.2f us, count: %10d%n", msg, mean, sdev, totalCount);
